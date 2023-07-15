c555dd4a3b99e407440ec463fc634c2f55f166ca
Joyce_Zhang <61525598+zyy159@users.noreply.github.com>
2023-07-15 13:33:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const SignUp = asyncComponent(() => import("./Component/SignUp"));
 const SignIn = asyncComponent(() => import("./Component/SignIn"));
 const Home = asyncComponent(() => import("./Component/Home"));
 const Yoga = asyncComponent(() => import("./Component/QuickStart_Yoga"));
-const Yoga2 = asyncComponent(() => import("./Component/QuickStart_Yoga2"));
 const ExerciseOption = asyncComponent(() => import("./Component/ExerciseOption"));
 const Working_Yoga = asyncComponent(() => import("./Component/Working_Yoga"));
 const MakeSchedule = asyncComponent(() => import("./Component/MakeSchedule"));
+const ResetPassword = asyncComponent(() => import("./Component/ResetPassword"));
 
 class App extends React.Component{
   render() {
@@ -22,10 +22,10 @@ class App extends React.Component{
           <Route path="/SignIn" element={<SignIn/>} />
           <Route path="/Home" element={<Home/>} />
           <Route path="/Yoga" element={<Yoga/>} />
-          <Route path="/Yoga2" element={<Yoga2/>} />
           <Route path="/ExerciseOption" element={<ExerciseOption/>} />
-          <Route path="/Working_Yoga" element={<Working_Yoga/>} />
           <Route path="/MakeSchedule" element={<MakeSchedule/>} />
+          <Route path="/ResetPassword" element={<ResetPassword/>} />
+          <Route path="/Working_Yoga" element={<Working_Yoga/>} />
         </Routes>
       </Router>
     );
