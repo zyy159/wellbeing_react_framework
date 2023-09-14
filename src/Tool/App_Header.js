3de5c550a28c55cc32dce4a02ab71941c8f53828
Stanven-1987 <79982058+Stanven-1987@users.noreply.github.com>
2023-09-15 07:45:31 +0800

    Update App_Header.js

--- src/Tool/App_Header.js
+++ src/Tool/App_Header.js
@@ -1,4 +1,4 @@
-import logo from '../Picture/HSBC-LOGO.png'
+// import logo from '../Picture/HSBC-LOGO.png'
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
@@ -18,125 +18,123 @@ import Logout from '@mui/icons-material/Logout';
 import axios from 'axios';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post['Content-Type'] = "application/json";
-const server = 'http://47.97.104.79/';
+const server = 'https://wellbeing.htcangelfund.com/api/';
 
 function AppHeader(props) {
-  const {setTopage} = props;
-  const [anchorEl, setAnchorEl] = React.useState(null);
+    const {setTopage} = props;
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const hadleHomePage = () => {
-    setTopage("Home")
-  }
-  const handleLogout = () => {
-    const csrftoken = cookie.load("csrftoken")
-    console.log(csrftoken)
-    let data = new FormData();
-    axios.post(server+"rest-auth/logout/", data,{headers:{'X-CSRFToken':csrftoken}}).then(function (response) {
-      cookie.remove('user_id')
-      cookie.remove('token')
-      cookie.remove('email')
-      cookie.remove('csrftoken')
-      setTopage("SignIn")
-    })
-  }
-
-  const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-      primary: {
-        main: '#1976d2'
-      }
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const hadleHomePage = () => {
+        setTopage("Home")
     }
-  })
-  if(!cookie.load('user_id')) {
-    return (
-      <div className="AppBar">
-        <ThemeProvider theme={darkTheme}>
-          <Box sx={{flexGrow: 1}}>
-            <AppBar position="static" color="primary" sx={{height: 60}}>
-              <Toolbar position="regular">
-                <img src={logo} className="App_Logo" alt="logo"/>
-                <Typography variant="h4" color="#EE270C" component="div" sx={{flexGrow: 1, fontFamily: 'HWE', ml: 1}}>
-                  Wellbeing Gallery
-                </Typography>
-              </Toolbar>
-            </AppBar>
-          </Box>
-        </ThemeProvider>
-      </div>
-    );
-  }else{
-    const user_id = cookie.load('user_id')
-    const email = cookie.load('email')
-    return (
-      <div className="AppBar">
-        <ThemeProvider theme={darkTheme}>
-          <Box sx={{flexGrow: 1, mb: 9}}>
-            <AppBar position="fixed" color="primary" sx={{height: 60}}>
-              <Toolbar position="regular">
-                <img src={logo} className="App_Logo" alt="logo"/>
-                <Typography variant="h4" color="#EE270C" component="div" sx={{flexGrow: 1, fontFamily: 'HWE', ml: 1}}>
-                  Wellbeing Gallery
-                </Typography>
-                <div>
-                <IconButton
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={hadleHomePage}
-                  color="inherit"
-                >
-                  <HomeIcon color={"error"} fontSize="large"/>
-                </IconButton>
-                <IconButton
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle color={"error"} fontSize="large"/>
-                </IconButton>
+    const handleLogout = () => {
+        const csrftoken = cookie.load("csrftoken")
+        console.log(csrftoken)
+        let data = new FormData();
+        axios.post(server+"rest-auth/logout/", data,{headers:{'X-CSRFToken':csrftoken}}).then(function (response) {
+            cookie.remove('user_id')
+            cookie.remove('token')
+            cookie.remove('email')
+            cookie.remove('csrftoken')
+            setTopage("SignIn")
+        })
+    }
 
-                  <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                      vertical: 'bottom',
-                      horizontal: 'center',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'center',
-                    }}
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                  >
-                    <MenuItem sx={{fontFamily: 'HWE'}}>Hi, {user_id}</MenuItem>
-                    <MenuItem sx={{fontFamily: 'HWE'}}>{email}</MenuItem>
-                    <Divider />
-                    <MenuItem onClick={handleLogout} sx={{fontFamily: 'HWE'}}>
-                      <ListItemIcon>
-                        <Logout fontSize="small" />
-                      </ListItemIcon>
-                      Logout
-                    </MenuItem>
-                  </Menu>
-                </div>
-              </Toolbar>
-            </AppBar>
-          </Box>
-        </ThemeProvider>
-      </div>
-    );
-  }
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+            primary: {
+                main: '#1976d2'
+            }
+        }
+    })
+    if(!cookie.load('user_id')) {
+        return (
+            <div className="AppBar">
+                <ThemeProvider theme={darkTheme}>
+                    <Box sx={{flexGrow: 1}}>
+                        <AppBar position="static" color="primary" sx={{height: 60}}>
+                            <Toolbar position="regular">
+                                <Typography variant="h4" color="#EE270C" component="div" sx={{flexGrow: 1, fontFamily: 'MSYH', ml: 1}}>
+                                    Wellbeing Gallery
+                                </Typography>
+                            </Toolbar>
+                        </AppBar>
+                    </Box>
+                </ThemeProvider>
+            </div>
+        );
+    }else{
+        const user_id = cookie.load('user_id')
+        const email = cookie.load('email')
+        return (
+            <div className="AppBar">
+                <ThemeProvider theme={darkTheme}>
+                    <Box sx={{flexGrow: 1, mb: 9}}>
+                        <AppBar position="fixed" color="primary" sx={{height: 60}}>
+                            <Toolbar position="regular">
+                                <Typography variant="h4" color="#EE270C" component="div" sx={{flexGrow: 1, fontFamily: 'MSYH', ml: 1}}>
+                                    Wellbeing Gallery
+                                </Typography>
+                                <div>
+                                <IconButton
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={hadleHomePage}
+                                    color="inherit"
+                                >
+                                    <HomeIcon color={"error"} fontSize="large"/>
+                                </IconButton>
+                                <IconButton
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleMenu}
+                                    color="inherit"
+                                >
+                                    <AccountCircle color={"error"} fontSize="large"/>
+                                </IconButton>
+
+                                    <Menu
+                                        id="menu-appbar"
+                                        anchorEl={anchorEl}
+                                        anchorOrigin={{
+                                            vertical: 'bottom',
+                                            horizontal: 'center',
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'center',
+                                        }}
+                                        open={Boolean(anchorEl)}
+                                        onClose={handleClose}
+                                    >
+                                        <MenuItem sx={{fontFamily: 'MSYH'}}>Hi, {user_id}</MenuItem>
+                                        <MenuItem sx={{fontFamily: 'MSYH'}}>{email}</MenuItem>
+                                        <Divider />
+                                        <MenuItem onClick={handleLogout} sx={{fontFamily: 'MSYH'}}>
+                                            <ListItemIcon>
+                                                <Logout fontSize="small" />
+                                            </ListItemIcon>
+                                            Logout
+                                        </MenuItem>
+                                    </Menu>
+                                </div>
+                            </Toolbar>
+                        </AppBar>
+                    </Box>
+                </ThemeProvider>
+            </div>
+        );
+    }
 }
 
 export default AppHeader;
