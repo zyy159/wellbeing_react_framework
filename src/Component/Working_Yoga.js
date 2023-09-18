4fb936194eb655bc0b77db03da3d6e30ff6e1be4
Stanven-1987 <79982058+Stanven-1987@users.noreply.github.com>
2023-09-18 02:30:30 -0500

    Update Working_Yoga.js

--- src/Component/Working_Yoga.js
+++ src/Component/Working_Yoga.js
@@ -251,31 +251,31 @@ function Working_Yoga(){
     useEffect(() => {
         async function loadPoseNetModel() {
       // 从 base64Weights.json 文件中获取 Base64 编码的权重字符串数组
-      const response = await fetch('https://wellbeing.htcangelfund.com/public/base64Weights.json');
-          const base64WeightsArray = await response.json();
-
-          // 将 Base64 编码的字符串解码为 ArrayBuffer
-          const weightBuffers = base64WeightsArray.map(base64Weights => {
-            const binaryWeights = Uint8Array.from(atob(base64Weights), c => c.charCodeAt(0));
-            return binaryWeights.buffer;
-          });
+//      const response = await fetch('https://wellbeing.htcangelfund.com/public/base64Weights.json');
+//          const base64WeightsArray = await response.json();
+//
+//          // 将 Base64 编码的字符串解码为 ArrayBuffer
+//          const weightBuffers = base64WeightsArray.map(base64Weights => {
+//            const binaryWeights = Uint8Array.from(atob(base64Weights), c => c.charCodeAt(0));
+//            return binaryWeights.buffer;
+//          });
 
           const model = await posenet.load({
             architecture: 'MobileNetV1',
             outputStride: 16,
             multiplier: 0.75,
             modelUrl: 'https://wellbeing.htcangelfund.com/public/models/movenet/model.json',
-            fetchFunc: async (url) => {
-              console.log(url)
-              const shardIndex = url.match(/shard(\d+)of(\d+)\.bin/);
-              if (shardIndex) {
-                const index = parseInt(shardIndex[1]) - 1;
-                console.log(weightBuffers[index])
-                return new Response(weightBuffers[index], { headers: { 'Content-Type': 'application/octet-stream' } });
-              } else {
-                return fetch(url);
-              }
-            }
+//            fetchFunc: async (url) => {
+//              console.log(url)
+//              const shardIndex = url.match(/shard(\d+)of(\d+)\.bin/);
+//              if (shardIndex) {
+//                const index = parseInt(shardIndex[1]) - 1;
+//                console.log(weightBuffers[index])
+//                return new Response(weightBuffers[index], { headers: { 'Content-Type': 'application/octet-stream' } });
+//              } else {
+//                return fetch(url);
+//              }
+//            }
           });
 
           setNet(model);
@@ -294,31 +294,31 @@ function Working_Yoga(){
             //console.log("Video data loaded.");
             async function initializePoseNet() {
                 // 从 base64Weights.json 文件中获取 Base64 编码的权重字符串数组
-                const response = await fetch('https://wellbeing.htcangelfund.com/public/base64Weights.json');
-                const base64WeightsArray = await response.json();
-
-                // 将 Base64 编码的字符串解码为 ArrayBuffer
-                const weightBuffers = base64WeightsArray.map(base64Weights => {
-                    const binaryWeights = Uint8Array.from(atob(base64Weights), c => c.charCodeAt(0));
-                    return binaryWeights.buffer;
-                });
+//                const response = await fetch('https://wellbeing.htcangelfund.com/public/base64Weights.json');
+//                const base64WeightsArray = await response.json();
+//
+//                // 将 Base64 编码的字符串解码为 ArrayBuffer
+//                const weightBuffers = base64WeightsArray.map(base64Weights => {
+//                    const binaryWeights = Uint8Array.from(atob(base64Weights), c => c.charCodeAt(0));
+//                    return binaryWeights.buffer;
+//                });
 
                 const net = await posenet.load({
                     architecture: 'MobileNetV1',
                     outputStride: 16,
                     multiplier: 0.75,
                     modelUrl: 'https://wellbeing.htcangelfund.com/public/models/movenet/model.json',
-                    fetchFunc: async (url) => {
-                      console.log(url)
-                      const shardIndex = url.match(/shard(\d+)of(\d+)\.bin/);
-                      if (shardIndex) {
-                        const index = parseInt(shardIndex[1]) - 1;
-                        console.log(weightBuffers[index])
-                        return new Response(weightBuffers[index], { headers: { 'Content-Type': 'application/octet-stream' } });
-                      } else {
-                        return fetch(url);
-                      }
-                    }
+//                    fetchFunc: async (url) => {
+//                      console.log(url)
+//                      const shardIndex = url.match(/shard(\d+)of(\d+)\.bin/);
+//                      if (shardIndex) {
+//                        const index = parseInt(shardIndex[1]) - 1;
+//                        console.log(weightBuffers[index])
+//                        return new Response(weightBuffers[index], { headers: { 'Content-Type': 'application/octet-stream' } });
+//                      } else {
+//                        return fetch(url);
+//                      }
+//                    }
                   });
 
 //                const net = await posenet.load({
