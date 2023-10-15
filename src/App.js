22e9488613b165b50d87053cfab2e01873b4c5e1
Stanven-1987 <79982058+Stanven-1987@users.noreply.github.com>
2023-10-15 12:00:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,23 +12,29 @@ const ExerciseOption = asyncComponent(() => import("./Component/ExerciseOption")
 const Working_Yoga = asyncComponent(() => import("./Component/Working_Yoga"));
 const MakeSchedule = asyncComponent(() => import("./Component/MakeSchedule"));
 const ResetPassword = asyncComponent(() => import("./Component/ResetPassword"));
+const PromotionSignUp = asyncComponent(() => import("./Component/PromotionSignUp"));
+const PrivacyPolicy = asyncComponent(() => import("./Component/PrivacyPolicy"));
+
+
 
 class App extends React.Component{
   render() {
-    return(
-      <Router history={history}>
-        <Routes>
-          <Route path="/SignUp" element={<SignUp/>} />
-          <Route path="/SignIn" element={<SignIn/>} />
-          <Route path="/Home" element={<Home/>} />
-          <Route path="/Yoga" element={<Yoga/>} />
-          <Route path="/ExerciseOption" element={<ExerciseOption/>} />
-          <Route path="/MakeSchedule" element={<MakeSchedule/>} />
-          <Route path="/ResetPassword" element={<ResetPassword/>} />
-          <Route path="/Working_Yoga" element={<Working_Yoga/>} />
-        </Routes>
-      </Router>
-    );
+        return(
+            <Router history={history}>
+                <Routes>
+                    <Route path="/SignUp" element={<SignUp/>} />
+                    <Route path="/Promotionsignup" element={<PromotionSignUp/>} />
+                    <Route path="/Privacypolicy" element={<PrivacyPolicy />} />
+                    <Route path="/SignIn" element={<SignIn/>} />
+                    <Route path="/Yoga" element={<Yoga/>} />
+                    <Route path="/ExerciseOption" element={<ExerciseOption/>} />
+                    <Route path="/MakeSchedule" element={<MakeSchedule/>} />
+                    <Route path="/ResetPassword" element={<ResetPassword/>} />
+                    <Route path="/Working_Yoga" element={<Working_Yoga/>} />
+                    <Route path="/" element={<Home/>} />
+                </Routes>
+            </Router>
+        );
   }
 }
 
